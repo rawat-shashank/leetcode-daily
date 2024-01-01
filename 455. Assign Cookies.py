4727670658135b3c4ec4da602933a2c671808e8c
rawat-shashank <shashank17nov@gmail.com>
2024-01-01 06:41:13 +0530

    01-01-24-easy

---
+++ 455. Assign Cookies.py
@@ -0,0 +1,39 @@
+# 455. Assign Cookies
+import unittest
+from typing import List
+
+
+class Solution:
+    def findContentChildren(slef, g: List[int], s: List[int]) -> int:
+        if len(s) == 0 or len(g) == 0:
+            return 0
+        g.sort()
+        s.sort()
+        l, r = 0, 0
+        while l < len(g) and r < len(s):
+            if g[l] <= s[r]:
+                l += 1
+                r += 1
+            else:
+                r += 1
+        return l
+
+
+class Testcases(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def testcase1(self):
+        self.assertEqual(
+            1, self.sol.findContentChildren(g=[1, 2, 3], s=[1, 1]))
+
+    def testcase2(self):
+        self.assertEqual(
+            2, self.sol.findContentChildren(g=[1, 2], s=[1, 2, 3]))
+
+    def testcase3(self):
+        self.assertEqual(1, self.sol.findContentChildren(g=[1, 2, 3], s=[3]))
+
+
+if __name__ == "__main__":
+    unittest.main()
