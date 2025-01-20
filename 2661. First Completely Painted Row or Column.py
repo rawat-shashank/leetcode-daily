258a093c65ba1aaa0aa6ce32b2be1f70b5a9e075
rawat-shashank <shashank17nov@gmail.com>
2025-01-20 11:50:28 +0000

    med - 2661 - hashmap

---
+++ 2661. First Completely Painted Row or Column.py
@@ -0,0 +1,48 @@
+import unittest
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        ROWS, COLS = len(mat), len(mat[0])
+
+        mat_pos = {}
+        for r in range(ROWS):
+            for c in range(COLS):
+                mat_pos[mat[r][c]] = (r, c)
+
+        row_counts = [0] * ROWS
+        col_counts = [0] * COLS
+
+        for i in range(len(arr)):
+            r, c = mat_pos[arr[i]]
+            row_counts[r] += 1
+            col_counts[c] += 1
+
+            if col_counts[c] == ROWS or row_counts[r] == COLS:
+                return i
+
+
+class Testcases(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def testcase1(self):
+        self.assertEqual(
+            2,
+            self.sol.firstCompleteIndex(arr=[1, 3, 4, 2], mat=[[1, 4], [2, 3]]),
+        )
+
+    def testcase2(self):
+        self.assertEqual(
+            3,
+            self.sol.firstCompleteIndex(
+                arr=[2, 8, 7, 4, 1, 3, 5, 6, 9], mat=[[3, 2, 5], [1, 4, 6], [8, 7, 9]]
+            ),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
