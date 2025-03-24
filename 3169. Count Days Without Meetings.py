64546e70f4398c52e0d86cc9192e1c3f746b50d2
rawat-shashank <shashank17nov@gmail.com>
2025-03-24 12:28:17 +0000

    medium - 3169 - sorting

---
+++ 3169. Count Days Without Meetings.py
@@ -0,0 +1,39 @@
+import unittest
+
+
+class Solution:
+    def countDays(self, days:int, meetings: list[list[int]]) -> str:
+        meetings.sort()
+        prev_end = 0
+        for start, end in meetings:
+            start = max(start, prev_end + 1)
+            length = end - start + 1
+            days -= max(length, 0 )
+            prev_end = max(prev_end, end)
+        return days
+
+
+class Testcases(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def testcase1(self):
+        self.assertEqual(
+            2,
+            self.sol.countDays(days = 10, meetings = [[5,7],[1,3],[9,10]]),
+        )
+
+    def testcase2(self):
+        self.assertEqual(
+            1,
+            self.sol.countDays(days = 5, meetings = [[2,4],[1,3]]),
+        )
+
+    def testcase3(self):
+        self.assertEqual(
+            0,
+            self.sol.countDays(days = 6, meetings = [[1,6]])
+        )
+
+if __name__ == "__main__":
+    unittest.main()
