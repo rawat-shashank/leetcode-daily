0544450c3ce22f7ea886a8b7d5f2ef3075b7a043
rawat-shashank <shashank17nov@gmail.com>
2025-04-30 08:54:01 +0100

    easy - 1295 - iteration

---
+++ 1295. Find Numbers with Even Number of Digits.py
@@ -0,0 +1,32 @@
+import unittest
+from typing import List
+
+
+class Solution:
+    def findNumbers(self, nums: list[int]) -> int:
+        res = 0
+        for num in nums:
+            if not len(str(num)) % 2:
+                res += 1
+        return res
+
+
+class Testcases(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def testcase1(self):
+        self.assertEqual(
+            2,
+            self.sol.findNumbers(nums=[12, 345, 2, 6, 7896]),
+        )
+
+    def testcase2(self):
+        self.assertEqual(
+            1,
+            self.sol.findNumbers(nums=[555, 901, 482, 1771]),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
